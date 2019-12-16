bee2558cf9d332a7b6c197587b7e5e92aaf48b36
Beliol123 <paul.mauconduit@etu.univ-lehavre.fr>
2019-12-16 16:29:51 +0100

    Changer le style du bouton

--- frontend/src/app/components/seance/Matiere.js
+++ frontend/src/app/components/seance/Matiere.js
@@ -4,15 +4,17 @@ import style from "./seance.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"  
 
-function activateClick (){
-    
-}
+function activateClick() {}
 
 export default function Matiere(props) {
-    return (
-        <div>
-            <Button onClick={activateClick} style={{backgroundColor: props.color}} >{props.name}</Button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Button
+        onClick={activateClick}
+        style={{ backgroundColor: props.color, borderColor: props.color }}
+      >
+        {props.name}
+      </Button>
+    </div>
+  );
 
