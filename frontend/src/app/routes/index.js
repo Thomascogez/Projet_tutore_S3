335a52dd4424f190593f5780b9a437fd036f9ea9
Jerome766 <jerome.laurent800@gmail.com>
2019-12-17 14:44:51 +0100

    ajout ajoutUser

--- frontend/src/app/routes/index.js
+++ frontend/src/app/routes/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Home from '../pages/home'
 import Seances from '../pages/seances'
 import Ajout from '../pages/ajout'
+import AjoutUser from '../pages/ajoutUser'
 
 const routes = {
     '/' : () => <Home />,
     '/seances' : () => <Seances />,
-    '/ajout' : () => <Ajout />
+    '/ajout' : () => <Ajout />,
+    '/ajoutUser' : () => <AjoutUser />
 }
 
 
