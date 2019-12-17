45a89f496ed699c5b6ac861e1cad187e4a224699
Jerome766 <jerome.laurent800@gmail.com>
2019-12-17 17:51:01 +0100

    mod ajoutUser

--- frontend/src/app/routes/index.js
+++ frontend/src/app/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from '../pages/home'
 import Seances from '../pages/seances'
 import Ajout from '../pages/seances/ajout'
+import AjoutUser from '../pages/ajoutUser'
 import TestModal from '../pages/PaulTests/modalTest'
 
 const routes = {
     '/' : () => <Home />,
     '/seances' : () => <Seances />,
     '/seances/ajout' : () => <Ajout />,
+    '/ajoutUser' : () => <AjoutUser />,
     '/modalTest' : () => <TestModal />
 }
 
