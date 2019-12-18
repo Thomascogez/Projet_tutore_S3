d8bc453f102545cf5c7d32cc4e355cd9f558e869
Thomascogez <thomas.cogez-allix@etu.univ-lehavre.fr>
2019-12-18 10:22:18 +0100

    modif user

---
+++ frontend/src/app/components/administration_components/utilisateurs/User.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Button, FormInput, ButtonGroup } from "shards-react";
+import { FaCheck, FaTimes } from "react-icons/fa";
+export default function User({ identifier, name, surname, groups, roles }) {
+  const [editing, setEditing] = useState(false);
+
+  const handleValidate = () => {
+    //when validate editing
+    setEditing(false);
+  };
+
+  const handleCancel = () => {
+    //when cancel editing
+    setEditing(false);
+  };
+  return (
+   
+      <tr>
+        <th scope="row">{identifier}</th>
+        <td>
+          {editing ? <FormInput value={name} placeholder="Nom ..." /> : name}
+        </td>
+        <td>
+          {editing ? (
+            <FormInput value={surname} placeholder="Prénom ..." />
+          ) : (
+            surname
+          )}
+        </td>
+        <td>
+          {editing ? <FormInput value={groups} placeholder="groups" /> : groups}
+        </td>
+        <td>
+          {editing ? <FormInput value={surname} placeholder="roles" /> : roles}
+        </td>
+        <td>
+          {editing ? (
+            <ButtonGroup>
+              <Button onClick={() => handleValidate()} theme="success">
+                <FaCheck />
+              </Button>
+              <Button onClick={() => handleCancel()} theme="danger">
+                <FaTimes />
+              </Button>
+            </ButtonGroup>
+          ) : (
+            <Button onClick={() => setEditing(true)}>Edition</Button>
+          )}
+        </td>
+      </tr>
+   
+  );
+}
--- frontend/src/app/routes/index.js
+++ frontend/src/app/routes/index.js
@@ -5,14 +5,15 @@ import Ajout from '../pages/seances/ajout'
 import AjoutUser from '../pages/ajoutUser'
 import TestModal from '../pages/PaulTests/modalTest'
 import Administration from '../pages/administration'
-
+import GererUtilisateur from '../pages/administration/utilisateurs'
 const routes = {
     '/' : () => <Home />,
     '/seances' : () => <Seances />,
     '/seances/ajout' : () => <Ajout />,
     '/ajoutUser' : () => <AjoutUser />,
     '/modalTest' : () => <TestModal />,
-    '/administration' : () =><Administration/>
+    '/administration' : () =><Administration/>,
+    '/administration/utilisateurs' : () =><GererUtilisateur/>
 }
 
 
