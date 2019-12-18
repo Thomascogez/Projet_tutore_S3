16f57fd4d193a2da68578dd950f137d3173a82c0
LeoPre <leo.prevost@etu.univ-lehavre.fr>
2019-12-18 13:57:26 +0100

    change

--- frontend/src/app/pages/seances/ajout/index.js
+++ frontend/src/app/pages/seances/ajout/index.js
@@ -35,90 +35,83 @@ export default function Ajout() {
     const handleClickAdd=() => {
         setAdd(true)
     }
-    
 
     return (
+    <Container fluid >
+        {/* <h1 className={style.title}>Ajouter une Séance</h1>
+        
+        <div className={ style.page }> 
+        <IoMdAddCircleOutline onClick={() => handleClickAdd()} className={style.add}/>
 
-        <Container fluid >
-            <h1 className={style.title}>Ajouter une Séance</h1>
-            <div className={ style.page }> 
-            <IoMdAddCircleOutline onClick={() => handleClickAdd()} className={style.add}/>
-            { add ?
-                <Row>
-                    <Col sm="12" lg="12">
-                        <form> 
-                            <FormSelect className= {style.AddSubject}>
-                                <option value="first"> Matière </option>
-                                <option value="Systeme">Systeme</option>
-                                <option value="third">E.C</option>
-                                <option value="fourth">Bado</option>
-                                <option value="fifth">CDIN</option>
-                                <option value="second">Systeme</option>
-                                <option value="third">E.C</option>
-                                <option value="fourth">Bado</option>
-                                <option value="fifth">CDIN</option>
-                            </FormSelect> 
+        <Row>
+            <Col sm="12" lg="12">
+                <form> 
+                    <FormSelect className= {style.AddSubject}>
+                        <option value="first"> Matière </option>
+                        <option value="Systeme">Systeme</option>
+                        <option value="third">E.C</option>
+                        <option value="fourth">Bado</option>
+                        <option value="fifth">CDIN</option>
+                        <option value="second">Systeme</option>
+                        <option value="third">E.C</option>
+                        <option value="fourth">Bado</option>
+                        <option value="fifth">CDIN</option>
+                    </FormSelect> 
 
-                            <FormSelect className= {style.AddClassType}>
-                                <option value="first">Type</option>
-                                <option value="second">TD</option>
-                                <option value="third">Amphi</option>
-                                <option value="fourth">TP</option>
-                            </FormSelect> 
-                        </form>
-                    </Col>
-                </Row>
-                
+                    <FormSelect className= {style.AddClassType}>
+                        <option value="first">Type</option>
+                        <option value="second">TD</option>
+                        <option value="third">Amphi</option>
+                        <option value="fourth">TP</option>
+                    </FormSelect> 
+                </form>
+            </Col>
+        </Row>
 
-                <div className={style.Content}> 
-                    <ButtonGroup className={style.Button} >
-                        <Button theme="success" onClick={() => handleCLickAfaire()}>A FAIRE</Button>
-                        <Button theme="success" onClick={() => handleCLickFait()}>FAIT</Button>
-                    </ButtonGroup>
-                    {click ? 
-                        <div>
-                            { statu ? 
-                                    <Form>
-                                        <FormGroup className={style.info}>
-                                            <label className={style.txt}>A faire pour le </label>
-                                            <FormInput className={style.date} placeholder=". . / . . / . . . ." />
-                                            
-                                        </FormGroup>
+        <div className={style.Content}> 
+            <ButtonGroup className={style.Button} >
+                <Button theme="success" onClick={() => handleCLickAfaire()}>A FAIRE</Button>
+                <Button theme="success" onClick={() => handleCLickFait()}>FAIT</Button>
+            </ButtonGroup>
+            {click ? 
+                <div>
+                     { statu ? <Form>
+                                <FormGroup className={style.info}>
+                                    <label className={style.txt}>A faire pour le </label>
+                                    <FormInput className={style.date} placeholder=". . / . . / . . . ." />
+                                    
+                                </FormGroup>
 
-                                        <FormGroup className={style.note}>
-                                            <label className={style.txt}> Note </label>
-                                            <FormTextarea maxlength="90" placeholder="90 caractère max"   />
-                                            
-                                            <input className={style.fileSelect} type="file"></input>
-                                        </FormGroup>
-                                        <Button className={style.button} theme="success">Ajouter</Button>
-                                    </Form>
-                                :
-                                    <Form>
-                                        <FormGroup className={style.info}>
-                                            <label className={style.txt}>Durée</label>
-                                            <FormInput className={style.duree} placeholder=". . : . ." />
-                                        </FormGroup>
-
-                                        <FormGroup className={style.note}>
-                                            <label className={style.txt}> Note </label>
-                                            <FormTextarea maxlength="90" placeholder="90 caractère max"/>
-                                            <input  className={style.fileSelect} type="file" />
-                                        </FormGroup>
-                                        <Button className={style.button} theme="success">Ajouter</Button>
-                                    </Form>
-                            }
-                        </div>
-                            :
-                        <div></div>
-                    }
-                    </div>
-                        <span className={style.triche} >  </span>
-
+                                <FormGroup className={style.note}>
+                                    <label className={style.txt}> Note </label>
+                                    <FormTextarea maxlength="90" placeholder="90 caractère max"   />
+                                    
+                                    <input className={style.fileSelect} type="file"></input>
+                                </FormGroup>
+                                <Button className={style.button} theme="success">Ajouter</Button>
+                             </Form>
                     :
-                    <div> </div>
+                        <Form>
+                            <FormGroup className={style.info}>
+                                <label className={style.txt}>Durée</label>
+                                <FormInput className={style.duree} placeholder=". . : . ." />
+                            </FormGroup>
+
+                            <FormGroup className={style.note}>
+                                <label className={style.txt}> Note </label>
+                                <FormTextarea maxlength="90" placeholder="90 caractère max"/>
+                                <input  className={style.fileSelect} type="file"></input>
+                            </FormGroup>
+                            <Button className={style.button} theme="success">Ajouter</Button>
+                        </Form>
             }
-            </div>
-        </Container>       
-    )
-}
+                </div>
+                :
+                <div></div>
+            }
+
+        </div>
+            <span className={style.bas} >  </span>
+        </div> */}
+        </Container>
+)}
