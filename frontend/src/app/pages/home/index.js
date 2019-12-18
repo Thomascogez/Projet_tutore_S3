3ab9d59121468e4c04f741139b101c818707ee61
Thomascogez <thomas.cogez-allix@etu.univ-lehavre.fr>
2019-12-18 01:21:50 +0100

    Animation

--- frontend/src/app/pages/home/index.js
+++ frontend/src/app/pages/home/index.js
@@ -2,37 +2,45 @@ import React from "react";
 import { Container, Form, FormGroup, FormInput, Button, InputGroup } from "shards-react";
 import logo from "../../assets/images/scShare_logo.png";
 import style from "./home.module.css";
+import { CSSTransition } from "react-transition-group";
+
 export default function Home() {
   return (
 
-
-    <Container fluid className={style.LoginContainer}>
-      {/* <div className={style.Polygon}></div> */}
-      <Container className={style.LoginFormContainer}>
-        <h1>Connexion</h1>
-        <Form>
-          <FormGroup>
-            <InputGroup className="mb-2">
-              <FormInput id="#username" placeholder="Nom d'utilisateur ..." />
-            </InputGroup>
-          </FormGroup>
-          <FormGroup>
-            <FormInput
-              type="password"
-              id="#password"
-              placeholder="Mot de passe ..."
-            />
-          </FormGroup>
-          <Button type="success">Connexion !</Button>
-        </Form>
-      </Container>
-      <div className={style.SiteLogo}>
-        <span>
-          <img src={logo} alt="logo" />
-          schoolshare
+    <CSSTransition
+      in={true}
+      appear={true}
+      timeout={1000}
+      classNames="fade"
+    >
+      <Container fluid className={style.LoginContainer}>
+        {/* <div className={style.Polygon}></div> */}
+        <Container className={style.LoginFormContainer}>
+          <h1>Connexion</h1>
+          <Form>
+            <FormGroup>
+              <InputGroup className="mb-2">
+                <FormInput id="#username" placeholder="Nom d'utilisateur ..." />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <FormInput
+                type="password"
+                id="#password"
+                placeholder="Mot de passe ..."
+              />
+            </FormGroup>
+            <Button type="success">Connexion !</Button>
+          </Form>
+        </Container>
+        <div className={style.SiteLogo}>
+          <span>
+            <img src={logo} alt="logo" />
+            schoolshare
         </span>
-      </div>
-    </Container>
+        </div>
+      </Container>
+    </CSSTransition>
 
   );
 }
