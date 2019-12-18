c178cb2e72e05187172e023790bd25d55373610a
Jerome766 <jerome.laurent800@gmail.com>
2019-12-18 15:46:44 +0100

    fix paul connard

--- frontend/src/app/routes/index.js
+++ frontend/src/app/routes/index.js
@@ -5,7 +5,6 @@ import Seances from '../pages/seances'
 import Ajout from '../pages/seances/ajout'
 import AddUser from '../pages/addUser'
 import AddUserPage2 from '../pages/addUser/page2'
-import TestModal from '../pages/PaulTests/modalTest'
 import Administration from '../pages/administration'
 import GererUtilisateur from '../pages/administration/utilisateurs'
 import EditModule from '../pages/administration/Module'
@@ -19,7 +18,6 @@ const routes = {
     '/seances/ajout' : () => <Ajout />,
     '/addUser' : () => <AddUser />,
     '/addUser/page2' : () => <AddUserPage2 />,
-    '/modalTest' : () => <TestModal />,
     '/administration' : () =><Administration/>,
     '/administration/editModule' : () => <EditModule />,
     '/administration/utilisateurs' : () =><GererUtilisateur/>
