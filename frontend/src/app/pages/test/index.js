a3a3aeb6886feb26d0ec5d4fc377c3a760c9a30d
Jerome766 <jerome.laurent800@gmail.com>
2019-12-19 15:01:13 +0100

    test

--- frontend/src/app/pages/test/index.js
+++ frontend/src/app/pages/test/index.js
@@ -6,11 +6,31 @@
 
 import React, { useState } from "react";
 import Toggle from '../../components/options_components/Toggle'
+import { FormSelect } from 'shards-react'
 
 export default function Ajout() {
+
+  const [status, setStatus] = useState( "Matière" );
+  const [status2, setStatus2] = useState( "Groupe" );
+
   return (
     <div>
         <Toggle text="Theme sombre"/>
+
+        <FormSelect onChange={(e) => setStatus(e.target.value)}>
+            <option value="Matière"> Matière </option>
+            <option value="Systeme">Systeme</option>
+            <option value="EC">E.C</option>
+            <option value="Bado">Bado</option>
+        </FormSelect>
+
+        { status != "Matière" ? <FormSelect onChange={(e) => setStatus(e.target.value)}>
+            <option value="Groupe"> Groupe </option>
+            <option value="Systeme">Systeme</option>
+            <option value="EC">E.C</option>
+            <option value="Bado">Bado</option>
+        </FormSelect> : <div></div>}
+        <span>{status}</span>
     </div>
   );
 }
