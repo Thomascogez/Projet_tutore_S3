28ecf2daf7b7f64a0db6a0a2f109e5d278661456
Thomascogez <thomas.cogez-allix@etu.univ-lehavre.fr>
2019-12-20 22:18:04 +0100

    api const

--- frontend/src/app/types/apiConst.js
+++ frontend/src/app/types/apiConst.js
@@ -1,16 +1,22 @@
-
 /**
  * All Api routes goes here
  */
 
 
-const PATH_API = "https://www.schoolshare.tools";
+const PATH_API = "https://schoolshare.tools";
 
+// --------------------   PASSWORD FORGET  ---------------------- //
 const PASSWORD_FORGET_GET = PATH_API + "/api/users/passwordReset";
 const PASSWORD_FORGET_VERIFY = PATH_API + "/api/users/passwordReset";
 const CHECK_STILL_VALID = PATH_API + "/api/users/my";
 
+// --------------------   USER LOGIN  ---------------------- //
 const LOGIN_CHECK = PATH_API + "/api/login_check";
+
+// -------------------- ADMIN  -------------------------//
+const ALL_GROUPS = PATH_API + "/api/groups";
+
+// --------------------   TEST  ---------------------- //
 const CHECK_IS_ADMIN = PATH_API + "/api/users/isAdmin";
 
 export {
@@ -18,5 +24,6 @@ export {
     PASSWORD_FORGET_VERIFY,
     CHECK_STILL_VALID,
     LOGIN_CHECK, 
-    CHECK_IS_ADMIN
+    CHECK_IS_ADMIN,
+    ALL_GROUPS
 };
