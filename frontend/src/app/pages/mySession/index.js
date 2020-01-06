3cc187506a20be5470eb8f7a85a65a373abe2e1b
Beliol123 <paul.mauconduit@etu.univ-lehavre.fr>
2020-01-06 17:26:14 +0100

    mes seances

--- frontend/src/app/pages/mySession/index.js
+++ frontend/src/app/pages/mySession/index.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {APIgetMySession} from '../../api/sessionFetch';
+import moment from 'moment'
 import style from './mySession.module.css';
 import {
     Container,
     Row,
     Col
 } from 'shards-react';
+import PageLoader from '../../components/layouts/loader';
 
 export default function MySession()
 {
@@ -13,8 +15,8 @@ export default function MySession()
 
     useEffect(() => {
         APIgetMySession()
-        .then(data => {
-            setMySession(data);
+        .then(data => {     
+            setMySession(data.data);            
         })
         .catch(err => console.log(err));
     }, [])
@@ -26,9 +28,43 @@ export default function MySession()
                     <Container className={style.Contain}>
                         <Row>
                             <Col>Date</Col>
-                            <Col>Modude</Col>
+                            <Col>Module</Col>
                             <Col>Groupe</Col>
                         </Row>
+                        { (Object.keys(mySession).length === 0)? (
+                            <PageLoader />
+                        ):(                            
+                            Object.entries(mySession). map(m => (
+                                <div>
+                                    {
+                                        Object.entries(m[1]). map(n => (
+                                            <Row>
+                                                {
+                                                    Object.entries(n[1]). map(o => (
+                                                        <div>
+                                                            {console.log(o[1])}
+                                                            <Col>{moment(o[1][0].createdAt).format('DD/MM/YYYY')}</Col>
+                                                            {Object.keys(o[1][0].module).map(q => (
+                                                                <Col>
+                                                                    { q[1]}
+                                                                </Col>
+                                                            ))}
+                                                            {Object.keys(o[1][0].groupe).map(q => (
+                                                                <Col>
+                                                                    {q[2]}
+                                                                </Col>
+                                                            ))}
+                                                        </div>
+                                                    ))
+                                                }
+                                            </Row>
+                                            
+                                        ))
+                                    }
+                                </div>
+                                
+                            ))
+                        )}
                     </Container>
                 </div>
             </div>
