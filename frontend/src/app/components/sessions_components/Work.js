b3688714c6c0a4e64a408d5a36fee372b4128846
Jerome766 <jerome.laurent800@gmail.com>
2020-01-07 14:26:23 +0100

    ok

--- frontend/src/app/components/sessions_components/Work.js
+++ frontend/src/app/components/sessions_components/Work.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import style from '../../pages/sessions/sessions.module.css'
 
 import { FaCheckCircle } from 'react-icons/fa'
-export default function Matiere({color, name, finished}) {
+export default function Matiere({color, name, duree, finished}) {
   const [open, setOpen] = useState(false);
   return (
     <div onClick={() => setOpen(!open)} className={style.Work} >
@@ -11,7 +11,7 @@ export default function Matiere({color, name, finished}) {
       {finished ? <div style={{float:"right",position:"relative", margin:"2px 5px 0px 0px", color:"green"}}><FaCheckCircle /></div>: ""}
       <div style={{borderLeftColor:color}} className={style.WorkContent}>
         <div style={finished ?{textDecoration:"line-through", color:"rgb(211,211,211)"} : {} } className={style.WorkTitle}>{name}</div>
-        <span style={finished ?{textDecoration:"line-through", color:"rgb(211,211,211)"} : {} }>durée 1h00</span>
+        <span style={finished ?{textDecoration:"line-through", color:"rgb(211,211,211)"} : {} }>durée {duree}</span>
         <span style={finished ?{textDecoration:"line-through",color:"rgb(211,211,211)"} : {fontWeight:"490"} }>1/4 restant</span>
       </div>        
     </div>
