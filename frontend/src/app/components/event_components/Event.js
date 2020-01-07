ab4d25fe4581a32563d9486affbf3f53298d872e
Jerome766 <jerome.laurent800@gmail.com>
2020-01-07 09:01:16 +0100

    ok

--- frontend/src/app/components/event_components/Event.js
+++ frontend/src/app/components/event_components/Event.js
@@ -5,51 +5,32 @@ import { FaFileAlt } from 'react-icons/fa';
 import moment from 'moment';
 import {Button, ButtonGroup} from "shards-react";
 import {FaCheck, FaTimes} from "react-icons/fa";
-import { FormSelect } from "shards-react";
+import ModalViewDetails from "../view_event_components/ModalViewDetails"
 
 export default function Event(props) {
 
-    
     const [editing, setEditing] = useState((props === null));
-    const [deleting, setDeleting] = useState(false);
-    const [type, setType] = useState(["Projet", "à faire", "fait"]);
     props = props.event;
-
-    console.log(props)
 
     const [name, setName] = useState(props.name)
     const [session, setSession] = useState(props.session)
     const duree = ('0' + Math.floor(props.duration) % 24).slice(-2) + 'h' + ((props.duration % 1)*60 + '0').slice(0, 2)
-  
-    const handleValidate = () => {
-        setEditing(true);
-    };
-    
-    const handleCancel = () => {
-        setEditing(false);
-    };
 
     return (
         <React.Fragment>
             <tr>
                 <td>
-                    {editing ?
-                        <FormSelect style={{width: "90px"}}>
-                            <option value={props.type}>{props.type}</option>
-                            {type.map(Type => Type === props.type ? <React.Fragment /> : <option key={Type} value={Type}>{Type}</option>)}
-                        </FormSelect>
+                    {props.type === "fait" ? 
+                    <Badge theme="info" style={{width: "50px"}}>{props.type}</Badge>
                     :
-                        props.type === "fait" ? 
-                        <Badge theme="info" style={{width: "50px"}}>{props.type}</Badge>
-                        :
-                        <Badge theme="warning" style={{width: "50px"}}>{props.type}</Badge>
+                    <Badge theme="warning" style={{width: "50px"}}>{props.type}</Badge>
                     }
                 </td>
                 <td>
                     <Badge theme="info" style={{backgroundColor: session.module.color, width: "70px"}}>[ {session.module.code}] </Badge> {session.module.name}
                 </td>
                 <td>{session.type}</td>
-                <td>{name}</td>
+                <td><ModalViewDetails text={name}/></td>
                 <td>{duree}</td>
                 <td>{moment(props.dueAt).format("DD/MM/YYYY")}</td>
                 <td>
@@ -64,10 +45,10 @@ export default function Event(props) {
                 <td>
                     {editing ? (
                         <ButtonGroup>
-                            <Button onClick={() => handleValidate()} theme="success">
+                            <Button onClick={() => setEditing(true)} theme="success">
                                 <FaCheck/>
                             </Button>
-                            <Button onClick={() => handleCancel()} theme="danger">
+                            <Button onClick={() => setEditing(false)} theme="danger">
                                 <FaTimes/>
                             </Button>
                         </ButtonGroup>
@@ -76,7 +57,7 @@ export default function Event(props) {
                             {(props != null)? (
                                 <React.Fragment>
                                     <Button onClick={() => setEditing(true)}>Edition</Button>
-                                    <Button onClick={() => setDeleting(!deleting)} theme="danger">Supprimer</Button>
+                                    <Button theme="danger">Supprimer</Button>
                                 </React.Fragment>
                             ):(<React.Fragment />)}
                         </ButtonGroup>
