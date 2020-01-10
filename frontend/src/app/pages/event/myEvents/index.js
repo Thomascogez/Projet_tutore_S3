c67383bee5cec32b04e32396b95144aacfb11049
BenjaminFrancois <benjamin.francois@etu.univ-lehavre.fr>
2020-01-10 01:44:47 +0100

    MyEvent loading

--- frontend/src/app/pages/event/myEvents/index.js
+++ frontend/src/app/pages/event/myEvents/index.js
@@ -3,19 +3,21 @@ import {Card, CardHeader, Container} from 'shards-react'
 import style from './myEvents.module.css';
 import {APIgetMyEvents} from '../../../api/event';
 import Collapse from "../../../components/layouts/Collapse/CollapseEvents";
+import PageLoader from "../../../components/layouts/loader";
+import moment from "moment";
 
 export default function MyEvents() {
 
     const [events, setEvents] = useState({})
     const [totalDuration, setTotalDuration] = useState(0);
+    const [loader, setLoader] = useState(false);
 
 
     useEffect(() => {
+        setLoader(true)
         APIgetMyEvents()
         .then(data =>{
-
-            
-
+            setLoader(false)
             let tmp = [];
 
             Object.entries(data.data).forEach(event => {
@@ -29,42 +31,39 @@ export default function MyEvents() {
                         tmp[m.session.id][0] = m.session
                         tmp[m.session.id][1].push(m)
                     }
-                    
 
                     if (m.duration) setTotalDuration(totalDuration => m.duration + totalDuration)
 
-                    //settest(test =>[...test.sort((a,b) => new moment(a.session.createdAt).format('YYYYMMDD') - new moment(b.session.createdAt).format('YYYYMMDD')), m])
-                    /*settest(test =>
-                        [...test.sort(function(a, b)
-                        {
-                            if(a.session.module.name < b.session.module.name) { return -1; }
-                            if(a.session.module.name > b.session.module.name) { return 1; }
-                            return 0;
-                        }),m])*/
                 })
             });
-            
+            console.log(tmp)
             setEvents(tmp)
             
         })
     }, [])
 
     return (
-        <Container fluid className={style.EventsContainer}>        
-            <Card>
-                <CardHeader><h5>Mes évènements </h5></CardHeader>
-                    {
-                        events.length > 0 &&
-                        events.map(session =>(
-                            <Collapse
-                                    title={session[0].module.name}
-                                    size={session[1].length}
-                                    events={session[1]}
-                                >
-                                </Collapse>
-                        ))}
-                
-            </Card>
-        </Container>
+        <>
+            <Container fluid className={style.EventsContainer}>
+                <Card>
+                    <CardHeader><h5>Mes évènements </h5></CardHeader>
+                        {
+                            events.length > 0 &&
+                            events.map(session =>(
+                                <Collapse
+                                        key={session[0].id}
+                                        title={(<>{session[0].module.name} <span style={{fontSize: "15px"}}>{moment(session[0].createdAt).format("DD/MM/YYYY")}</span></>)}
+                                        size={session[1].length}
+                                        events={session[1]}
+                                    >
+                                    </Collapse>
+                            ))}
+
+                </Card>
+            </Container>
+            {loader &&
+            <PageLoader />
+            }
+        </>
     )
 }
